f8ed30be86f91d0af01fb9ee151bf7b0e189fd98
Harshit <harshit19051915@gmail.com>
2020-05-06 16:19:20 +0000

    Add register routes and add register forms

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -18,12 +18,19 @@ app.use(require('express-session')({
     saveUninitialized:false
 }));
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser(User.serializeUser()); //to encode sessions
 passport.deserializeUser(User.deserializeUser()); //to decode sessions
+
+//=================================================================================================================
+//ROUTES
+//=================================================================================================================
+
 
 app.get("/",function(req,res){
     res.render("home");
@@ -33,7 +40,29 @@ app.get("/secret",function(req,res){
     res.render("secret");
 });
     
+//AUTH ROUTES
 
+//show sign up form
+app.get("/register",function(req,res){
+   res.render("register"); 
+});
+
+//handling user sign in
+app.post("/register",function(req,res){
+    var username = req.body.username;
+    var password = req.body.password;
+    
+    //be careful new thing coming
+    User.register(new User({username:username}),password,function(err,user){
+        if(err){
+           console.log(err);
+           return res.render("register");
+        }
+        passport.authenticate("local")(req,res,function(){
+            res.redirect("/secret");
+        })
+    });
+});
 
 
 app.listen(3000,function(req,res){
