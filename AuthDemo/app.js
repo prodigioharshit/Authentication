72bb53750a7ac7504932ea5fe5f2b436c0f22423
Harshit <harshit19051915@gmail.com>
2020-05-06 15:32:18 +0000

    create models/user.js and add require methods and features in app.js for passport

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,16 +1,29 @@
-var express = require('express'),
-    mongoose = require('mongoose'),
-    app = express();
+var express                   = require('express'),
+    mongoose                  = require('mongoose'),
+    passport                  = require('passport'),
+    bodyParser                = require('body-parser'),
+    User                      = require('./models/user'),
+    LocalStrategy             = require('passport-local'),
+    passportLocalMongoose     = require('passport-local-mongoose'),
+    app                       = express();
      
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+
 mongoose.connect("mongodb://localhost/Secret_Page");
 
-var SecretPageschema = new mongoose.Schema({
-    
-})
-    
-var Secret = mongoose.model("secret",SecretPageschema);
+app.use(require('express-session')({
+    secret:"Never make promises when you are happy", //to encode and decode sessions
+    resave:false,
+    saveUninitialized:false
+}));
 
 app.set("view engine","ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser()); //to encode sessions
+passport.deserializeUser(User.deserializeUser()); //to decode sessions
 
 app.get("/",function(req,res){
     res.render("home");
